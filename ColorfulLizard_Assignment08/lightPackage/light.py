dbcdf20c51c4d7b89dfa31b1383cc056c9b3460a
Nandini Agarwal <nandiniagarwal1710@gmail.com>
2024-10-30 19:11:34 -0400

    added code in light.py

--- ColorfulLizard_Assignment08/lightPackage/light.py
+++ ColorfulLizard_Assignment08/lightPackage/light.py
@@ -1,3 +1,16 @@
+# Name: {required}
+# email:  {required}
+# Assignment Number: Assignment nn  {required}
+# Due Date:   {required}
+# Course #/Section:   {required}
+# Semester/Year:   {required}
+# Brief Description of the assignment:  {required}
+
+# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+# Citations:
+# Anything else that's relevant:
+
+ 
 #light.py
 
 class SmartLight:
